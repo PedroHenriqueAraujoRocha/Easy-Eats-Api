aaef88f6eb8ec01d6b602c5faa6741867161d07b
Pedro Henrique <pedro_ha_rocha@estudante.sesisenai.org.br>
2026-06-16 19:51:45 -0300

    Ajuste fornecedor getter e setter

--- src/main/java/com/easy/eats/fornecedor/model/Fornecedor.java
+++ src/main/java/com/easy/eats/fornecedor/model/Fornecedor.java
@@ -6,13 +6,17 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "TBFORNECEDOR")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
+@Setter
 public class Fornecedor {
 
     @Id
